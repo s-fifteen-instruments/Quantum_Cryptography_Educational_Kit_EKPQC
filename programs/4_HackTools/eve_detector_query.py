9714ff056b19d6c974a9a6cd9db8058d394b2f0f
JunHerng <52197879+JunHerng@users.noreply.github.com>
2023-02-17 15:14:08 +0800

    ctrl+c option added

--- programs/4_HackTools/eve_detector_query.py
+++ programs/4_HackTools/eve_detector_query.py
@@ -1,10 +1,18 @@
 """Program to continuously query Eve's detector readings.
-Useful for callibration and testing purposes.
+Useful for callibration and testing purposes. Program runs
+indefinitely - use 'Ctrl + C' to stop program.
+
+Author: JH
+Date: Feb 2023
+
+Sample command in windows CMD:
+> python eve_detector_query.py --serial com11
 """
 
 import serial
 import time
 import argparse # For running the script with options
+import sys
 
 # Select device com port
 my_parser = argparse.ArgumentParser()
@@ -17,14 +25,18 @@ timeout = 0.1
 dev = serial.Serial(ser,baudrate,timeout=timeout)
 
 def main():
-    while True:
-        # Query device
-        dev.write('volts? '.encode())
-        time.sleep(0.1)
-        # Get reply - repeat
-        reply = dev.readlines()
-        vals = [val.decode().strip() for val in reply]
-        print(vals)
+    try:
+        while True:
+            # Query device
+            dev.write('volts? '.encode())
+            time.sleep(0.1)
+            # Get reply - repeat
+            reply = dev.readlines()
+            vals = [val.decode().strip() for val in reply]
+            print(vals)
+    except KeyboardInterrupt:
+        print('Program closed.')
+        sys.exit()
 
 if __name__=='__main__':
     main()
